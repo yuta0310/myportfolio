5ba2ea77561ba94a6cbb6f8f7925e4f8622c8d6f
yuta0310 <yuta0310.2525@gmail.com>
2019-07-27 17:34:59 +0900

    チュートリアル

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ hello }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,7 +32,7 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    hello: String
   }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
+   <HelloWorld hello="ようこそ"/>
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
